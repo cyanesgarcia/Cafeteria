555f1ef1203f294fc0817fc9ddd5a2955ca56222
ClaudiaLidia <lidiayanesgarcia@gmail.com>
2020-10-04 13:13:07 +0100

    Listview

--- app/src/main/java/com/example/cafeteria/SnacksFragment.java
+++ app/src/main/java/com/example/cafeteria/SnacksFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,8 +14,9 @@ public class SnacksFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_snacks, container, false);
+        View view = inflater.inflate(R.layout.fragment_snacks, container, false);
+        ListView listview = (ListView) view.findViewById(R.id.listview);
 
-        return root;
+        return view;
     }
 }
